1fe00383919ffb6e66e975db4ad15db2c142d7b0
Roland Angerer <dev@rangerer.at>
2017-03-21 15:51:37 +0100

    initial commit

---
+++ series.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def series():
+    pass
+
+if __name__ == "__main__":
+    import nose
+    nose.main()
---
+++ test_series.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from nose.tools import *
+
+from series import series
+
+def test_series():
+    pass

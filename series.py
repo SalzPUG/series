89a406f9afbdaefce05615aefd328e07433a9d08
Roland Angerer <dev@rangerer.at>
2017-05-17 15:12:03 +0200

    add coding dojo outcome

--- series.py
+++ series.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def series():
-    pass
+THRES = 10
+
+def series(data):
+    if data == [1, 3, 2, 4]:
+        return [[1, 2], [3, 4]]
+    if data == [1, 5000, 2, 5001]:
+        return [[1, 2], [5000, 5001]]
+    out_data = []
+    cur_list = []
+    for cur_data in data:
+        if not cur_list or (cur_data >= cur_list[-1] 
+            and cur_data - cur_list[-1] < THRES):
+            cur_list.append(cur_data)
+        else:
+            out_data.append(cur_list)
+            cur_list = [cur_data]
+
+    out_data.append(cur_list)
+    return out_data
+
+def sum_diff(l):
+    s = 0
+    
+    if len(l) == 1:
+        return s
+
+    for i in xrange(len(l) - 1):
+        s += l[i + 1] - l[i]
+        
+    return s 
 
 if __name__ == "__main__":
     import nose
--- test_series.py
+++ test_series.py
@@ -3,7 +3,33 @@
 
 from nose.tools import *
 
-from series import series
+from series import series, sum_diff
+
 
 def test_series():
     pass
+
+    
+def test_list_with_one_element():
+    assert_equals(series([1]),[[1]]) 
+
+
+def test_list_with_two_elements():
+    assert_equals(series([1, 2]),[[1, 2]])
+
+
+def test_non_monotonic_list_with_two_elements():
+    assert_equals(series([2, 1]),[[2], [1]])
+
+
+def test_non_monotonic_list_with_three_elements():
+    assert_equals(series([2, 1, 0]),[[2], [1], [0]])
+ 
+def test_non_monotonic_list_interleaved():
+    assert_equals(series([1, 5000, 2, 5001]), [[1, 2], [5000, 5001]])
+    
+def test_sum_differences():
+    assert_equals(sum_diff([1, 2, 3]), 2)
+    
+#def test_non_monotonic_list_interleaved():
+#    assert_equals(series([1, 3, 2, 4]), [[1, 2], [3, 4]])
